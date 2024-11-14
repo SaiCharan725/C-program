1304ca3135d24e23390707eb6b704a700ce17d4b
G. Venkata Sai Charan <charan07052005@gmail.com>
2024-11-14 08:02:31 +0530

    Add files via upload

---
+++ percentage of marks.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float x1 = 90; // X1 can be physics
+	float x2 = 91; // x2 can be maths
+	float x3 = 93; //x3 can be sanskrit
+	float x4 = 95;// x4 can be physics
+	float x5 = 97;// x5 can be chemistry
+	float percent =( x1 + x2+ x3 +x4 + x5)/5;
+	printf("%f",percent);
+	return 0;
+}
